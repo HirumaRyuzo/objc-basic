d83cb3343b288e8497479b7f806c5456efd290a0
比留間龍三 <ryusan1215@gmail.com>
2018-06-30 13:41:24 +0900

    first commit

---
+++ stv_project2.1.2/stv_project2.1.2/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  stv_project2.1.2
+//
+//  Created by 比留間　龍三 on 2018/06/15.
+//  Copyright © 2018年 h. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ stv_project2.1.2/stv_project2.1.2/ViewController.m
@@ -0,0 +1,66 @@
+//
+//  ViewController.m
+//  stv_project2.1.2
+//
+//  Created by 比留間　龍三 on 2018/06/15.
+//  Copyright © 2018年 h. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//    CGFloat imageMargin = 20;
+//    CGFloat width = [UIScreen mainScreen].bounds.size.width - imageMargin * 2;
+//
+//    UIImage *image = [UIImage imageNamed:@"mm2"];
+//    CGFloat imageWidth = image.size.width;
+//    CGFloat imageHeight = image.size.height;
+//
+//    CGFloat w = width / imageWidth;
+//    CGFloat h = imageHeight * w;
+//
+//    // 画像リサイズ
+//    UIImage * resizeImage = [image btk_ImageResized:CGSizeMake(width, h)];
+//
+//    //UIImageViewの生成
+//    UIImageView *imageView = [[UIImageView alloc] initWithImage: resizeImage];
+//    imageView.frame = CGRectMake(20, 100, width, h);
+//    imageView.contentMode = UIViewContentModeScaleAspectFit;
+//    [self.view addSubview:imageView];
+//
+//    [imageView layer].masksToBounds = YES;
+//    [imageView layer].cornerRadius = 10;
+}
+@end
+
+
+@implementation UIImage (Additional)
+//
+//- (UIImage*) btk_ImageResized : (CGSize)size
+//{
+//    if(CGSizeEqualToSize(self.size, size) && self.imageOrientation == UIImageOrientationUp){
+//        // No need to resize nor convert orientation
+//        return self;
+//    }
+//    UIGraphicsBeginImageContext(size);
+//    @try {
+//        CGContextRef context = UIGraphicsGetCurrentContext();
+//        CGContextSetInterpolationQuality(context, kCGInterpolationHigh);
+//        [self drawInRect:CGRectMake(0, 0, size.width, size.height)];
+//        return UIGraphicsGetImageFromCurrentImageContext();
+//    }
+//    @finally {
+//        UIGraphicsEndImageContext();
+//    }
+//}
+@end
