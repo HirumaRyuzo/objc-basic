b0fd22ffa46b13f854258c31de39932991a871af
比留間龍三 <ryusan1215@gmail.com>
2018-07-03 18:15:15 +0900

    first commit

---
+++ stv_project2.1.8/stv_project2.1.8/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  stv_project2.1.8
+//
+//  Created by 比留間龍三 on 2018/07/03.
+//  Copyright © 2018年 比留間龍三. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
