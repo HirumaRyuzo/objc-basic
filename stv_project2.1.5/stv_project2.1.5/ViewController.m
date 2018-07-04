ab218a4924c3c2b17e7f37347c304c24ef06a971
比留間龍三 <ryusan1215@gmail.com>
2018-07-04 18:17:45 +0900

    [update]#2.1.5 変更しました

--- stv_project2.1.5/stv_project2.1.5/ViewController.m
+++ stv_project2.1.5/stv_project2.1.5/ViewController.m
@@ -21,14 +21,18 @@
     
     // 上から順にボタンが配置
     [alertController addAction:[UIAlertAction actionWithTitle:@"Facebook" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        [self selectedActionWith:1];
+//        [self selectedActionWith:1];
+         NSLog(@"Facebook");
     }]];
+    
     [alertController addAction:[UIAlertAction actionWithTitle:@"twitter" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         [self selectedActionWith:2];
     }]];
+    
     [alertController addAction:[UIAlertAction actionWithTitle:@"LINE" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         [self selectedActionWith:3];
     }]];
+    
     [alertController addAction:[UIAlertAction actionWithTitle:@"クリア" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         [self selectedActionWith:0];
     }]];
@@ -37,13 +41,11 @@
 //    alertController.popoverPresentationController.sourceView = self.view;
 //    alertController.popoverPresentationController.sourceRect = CGRectMake(_actionSheetBtn.frame.origin.x, _actionSheetBtn.frame.origin.y, 20.0, 20.0);
 //    alertController.popoverPresentationController.sourceView = _actionSheetBtn; //でも良い?
-    
     [self presentViewController:alertController animated:YES completion:nil];
-    
 }
 
 -(void)selectedActionWith:(int)index{
-    NSLog(@"選択: %d",index);
+    NSLog(@"%d", index);
     // 選択時の処理    
 }
 @end
