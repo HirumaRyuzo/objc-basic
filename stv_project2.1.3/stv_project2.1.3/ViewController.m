161628f77d06031c663eb0f05b70aa2f168a32d9
比留間龍三 <ryusan1215@gmail.com>
2018-07-04 17:53:35 +0900

    [update]#2.1.3 更新しました

--- stv_project2.1.3/stv_project2.1.3/ViewController.m
+++ stv_project2.1.3/stv_project2.1.3/ViewController.m
@@ -9,22 +9,26 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property NSArray<UIImage *> *view2;
+@property NSArray<NSString *> *viewList;
 @end
 
 @implementation ViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view2 = @[@"Image_0", @"Image_1", @"Image_2", @"Image_3"];
-    NSLog(@"%@", self.view2);
+    
+    self.viewList = @[@"Image_0", @"Image_1", @"Image_2", @"Image_3"];
+    NSLog(@"%@", self.viewList);
 }
 //ヘッダにIBActionで接続するとこっちにも自動にあらわれるので、
 //直接こっちにつなげてもよいけど他のクラスでアクセスはできなくなるのでヘッダーに接続したほうが確実
 - (IBAction)tap:(id)sender {//ボタンがタップされた時の処理
+    
     int rNum = arc4random_uniform(4);
-    NSLog(@"%@", self.view2);
+    NSLog(@"%@", self.viewList);
     NSLog(@"%ld", (long)rNum);
-    NSLog(@"%@", self.view2[rNum]);
-    self.imageView.image = [UIImage imageNamed:self.view2[rNum]];
+    NSLog(@"%@", self.viewList[rNum]);
+    
+    self.imageView.image = [UIImage imageNamed:self.viewList[rNum]];
 }
+
 @end
