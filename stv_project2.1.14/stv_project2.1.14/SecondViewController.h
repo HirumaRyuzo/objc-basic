d2b2c1ad4d4043f7098224676cb688c3fc94c1b4
比留間龍三 <ryusan1215@gmail.com>
2018-07-15 12:14:30 +0900

    [update]2.1.14 更新致しました。

---
+++ stv_project2.1.14/stv_project2.1.14/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  stv_project2.1.14
+//
+//  Created by 比留間龍三 on 2018/07/15.
+//  Copyright © 2018年 比留間龍三. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
