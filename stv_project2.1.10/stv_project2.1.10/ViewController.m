06d76170c025c954b2dc74a9f4cff30ca94904b3
比留間龍三 <ryusan1215@gmail.com>
2018-07-18 14:41:51 +0900

    [update] 2.1.10 更新しました

--- stv_project2.1.10/stv_project2.1.10/ViewController.m
+++ stv_project2.1.10/stv_project2.1.10/ViewController.m
@@ -8,121 +8,69 @@
 
 #import "ViewController.h"
 
-typedef NS_ENUM(NSUInteger, Class){
-    cell0 = 0,
-    cell1,
-};
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 
-@interface ViewController ()
-@property NSArray *cell0;
-@property NSArray *cell1;
-@property UIImageView *plist;
+@property (weak, nonatomic) IBOutlet UITableView *cellTable;
+
+@property (nonatomic) NSArray *DName;
+@property (nonatomic) NSArray *DImg;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-//    self.item0 = @[@"img0.JPG",@"img1.JPG",@"img2.JPG",@"img3.JPG",
-//                 @"img4.JPG",@"img5.JPG",@"img6.JPG",@"img7.JPG"];
-//    self.item1= @[@"2013/8/23/16:04",@"2013/8/23/16:15",@"2013/8/23/16:47",@"2013/8/23/17:10",
-//                    @"2013/8/23/1715:",@"2013/8/23/17:21",@"2013/8/23/17:33",@"2013/8/23/17:41"];
     
-//    //プロジェクト内のファイルにアクセスできるオブジェクトを宣言
-//    NSBundle *bundle = [NSBundle mainBundle];
-//    //読み込むプロパティリストのファイルパスを指定
-//    NSString *path = [bundle pathForResource:@"imageList" ofType:@"plist"];
-//    //プロパティリストの中身データを取得
-//    NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
-//    NSArray *cell0 = [dic objectForKey:@"cell0"];
-//    NSArray *cell1 = [dic objectForKey:@"cell1"];
-//
-//    //取得できた配列データをメンバ変数に代入
-//    self.cell0 = cell0;
-//    self.cell1 = cell1;
-
-//    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"imageList.plist" ofType:nil];
-//    NSData *plistData = [NSData dataWithContentsOfFile:filePath];
-//    NSPropertyListFormat format = NSPropertyListXMLFormat_v1_0;
-//    NSError *error;
-//    id lists = [NSPropertyListSerialization propertyListWithData:plistData options:(NSPropertyListReadOptions)NSPropertyListImmutable format:&format error:&error];
-//    if (!lists) {
-//        return;
-//    }
-//    for (NSDictionary *list in lists) {
-//        NSLog(@"imageName:%@", list[@"imageName"]);
-//        NSLog(@"label1:%@", list[@"label1"]);
-//        NSLog(@"label2:%@", list[@"label2"]);
-//    }
-
-//    _tableView.estimatedRowHeight = 50;  高さ
-    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    // Assetから写真をarrayに代入
+    self.DImg = @[[UIImage imageNamed:@"img0"],
+                       [UIImage imageNamed:@"img1"],
+                       [UIImage imageNamed:@"img4"]];
+    
+    // plistとbundleする
+    NSBundle *bundle  = [NSBundle mainBundle];
+    // ファイルパスを通す
+    NSString *path    = [bundle pathForResource:@"dList" ofType:@"plist"];
+    // plistのデータを取得
+    NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
+    NSArray *disney   = [dic objectForKey:@"disney"];
+    // インスタンス変数に代入する
+    self.DName = disney;
 }
 
-//Table Viewのセクション数を指定
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // セクション数
-    return 2;
+// cellを可変にする
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = (UITableViewCell *)[self tableView:self.cellTable cellForRowAtIndexPath:indexPath];
+    
+    return cell.frame.size.height;
 }
 
-//Table Viewのセルの数を指定
+// セクションの数を指定
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+// セルの数を指定 要素の数によって可変にする
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    // 今回は要素８個
-    return self.cell0.count;
+    return [self.DImg count];
 }
 
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
-    // セクションタイトルの文字列変数を宣言
-    NSString *title;
+// 各セルの要素を指定 tagで指定する
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // テーブルセルの IDで インスタンスを生成
+    static NSString *cellIdentifier = @"cell";
     
-    // 表示しているセクションのタイトルを
-    switch (section) {
-        case cell0:
-            title = @"section1";
-            break;
-        case cell1:
-            title = @"section2";
-            break;
-        default:
-            break;
-    }
-    return title;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    UIImage *img = self.DImg[indexPath.row];
+    
+    UIImageView *imageView = (UIImageView *)[cell viewWithTag:1];
+    imageView.image = img;
+    
+    UILabel *label = (UILabel *)[cell viewWithTag:2];
+    label.text = self.DName[indexPath.row];
+    
+    return cell;
 }
-
-////各セルの要素を設定する
-//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-////    static NSString *CellIdentifier = @"tableCell";
-//
-//    // tableCell の ID で UITableViewCell のインスタンスを生成
-//    UITableViewCell *cell =
-//    [tableView dequeueReusableCellWithIdentifier:@"tableCell"];
-//
-////    if(cell==nil){
-////        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier: @"tableCell"];
-////    }
-//
-//    UIImage *img = [UIImage imageNamed:self.item0[indexPath.row]];
-//    // Tag番号 1 で UIImageView インスタンスの生成
-//    self.plist = (UIImageView *)[cell viewWithTag:1];
-//    self.plist.image = img;
-//
-//    // Tag番号 ２ で UILabel インスタンスの生成
-//    UILabel *label1 = (UILabel *)[cell viewWithTag:2];
-//    label1.text = [NSString stringWithFormat:@"No.%d",(int)(indexPath.row+1)];
-//
-//    // Tag番号 ３ で UILabel インスタンスの生成
-//    UILabel *label2 = (UILabel *)[cell viewWithTag:3];
-//    label2.text = self.item1[indexPath.row];
-//
-//    return cell;
-//}
-
-//// セルの高さを設定する
-//- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-//    float height = 120.0f;
-//
-//    return height;
-//}
 
 @end
