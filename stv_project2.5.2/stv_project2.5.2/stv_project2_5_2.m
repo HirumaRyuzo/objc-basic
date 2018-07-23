37703d6e1122dbdb423cea2987839cc63fb5e10e
比留間龍三 <ryusan1215@gmail.com>
2018-07-23 12:04:51 +0900

    first commit

---
+++ stv_project2.5.2/stv_project2.5.2/stv_project2_5_2.m
@@ -0,0 +1,13 @@
+//
+//  stv_project2_5_2.m
+//  stv_project2.5.2
+//
+//  Created by 比留間龍三 on 2018/07/23.
+//  Copyright © 2018年 比留間龍三. All rights reserved.
+//
+
+#import "stv_project2_5_2.h"
+
+@implementation stv_project2_5_2
+
+@end
