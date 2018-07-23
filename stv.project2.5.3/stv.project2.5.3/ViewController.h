5c72f8cde28775596e3ce85678d676d9afb97959
比留間龍三 <ryusan1215@gmail.com>
2018-07-23 19:15:50 +0900

    first commit

---
+++ stv.project2.5.3/stv.project2.5.3/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  stv.project2.5.3
+//
+//  Created by 比留間龍三 on 2018/07/23.
+//  Copyright © 2018年 比留間龍三. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
