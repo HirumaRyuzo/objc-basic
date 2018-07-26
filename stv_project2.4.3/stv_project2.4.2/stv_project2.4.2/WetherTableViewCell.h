b123d6be3364c9b0b94b97ed1a5c6cabea833e29
比留間龍三 <ryusan1215@gmail.com>
2018-07-26 14:07:20 +0900

    [update]2.4.3 レビュー指摘箇所の修正致しました。

---
+++ stv_project2.4.3/stv_project2.4.2/stv_project2.4.2/WetherTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  WetherTableViewCell.h
+//  stv_project2.4.2
+//
+//  Created by 比留間龍三 on 2018/07/19.
+//  Copyright © 2018年 比留間龍三. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WetherTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *cellDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cellTelop;
+@property (weak, nonatomic) IBOutlet UILabel *cellText;
+@property (weak, nonatomic) IBOutlet UIImageView *cellImg;
+@end
---
+++ stv_project2.4.3/stv_project2.4.2/stv_project2.4.2/WetherTableViewCell.m
@@ -0,0 +1,20 @@
+//
+//  WetherTableViewCell.m
+//  stv_project2.4.2
+//
+//  Created by 比留間龍三 on 2018/07/19.
+//  Copyright © 2018年 比留間龍三. All rights reserved.
+//
+
+#import "WetherTableViewCell.h"
+@implementation WetherTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
