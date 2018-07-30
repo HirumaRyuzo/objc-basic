949bb5af65f7aebafc2b3d03a0cb31d2809f82e0
比留間龍三 <ryusan1215@gmail.com>
2018-07-30 14:52:31 +0900

    [update]2.3.6 更新致しました。

---
+++ stv_project2.3.6/stv_project 2.3.4/CustomTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.h
+//  stv_project2.3.4
+//
+//  Created by 比留間龍三 on 2018/07/30.
+//  Copyright © 2018年 比留間龍三. All rights reserved.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *readTitle;
+@property (weak, nonatomic) IBOutlet UILabel *readLimit;
+
+@end
---
+++ stv_project2.3.6/stv_project 2.3.4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  stv_project 2.3.4
+//
+//  Created by 比留間龍三 on 2018/07/29.
+//  Copyright © 2018年 比留間龍三. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@end
+
