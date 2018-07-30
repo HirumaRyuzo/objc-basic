3512ec7710541152522ddb8e8d051c17f68f8735
比留間龍三 <ryusan1215@gmail.com>
2018-07-30 15:05:50 +0900

    [update]2.3.1 命名規則通りに変数名を更新致しました。

--- stv_project2.3.1/stv_project2.3.1/ViewController.m
+++ stv_project2.3.1/stv_project2.3.1/ViewController.m
@@ -16,19 +16,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];  // 取得
-    [ud setInteger:111 forKey:@"KEY_A"];  // int型の100をKEY_Iというキーで保存
-    [ud setDouble:1.13498749827 forKey:@"KEY_B"];  // double型の1.23をKEY_Dというキーで保存
-    [ud setObject:@"ああ" forKey:@"KEY_C"];  // "あいう"をKEY_Sというキーで保存
-    [ud synchronize];  // NSUserDefaultsに即時反映させる（即時で無くてもよい場合は不要）
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];  // 取得
+    [userDefaults setInteger:111 forKey:@"KEY_A"];  // int型の100をKEY_Iというキーで保存
+    [userDefaults setDouble:1.13498749827 forKey:@"KEY_B"];  // double型の1.23をKEY_Dというキーで保存
+    [userDefaults setObject:@"ああ" forKey:@"KEY_C"];  // "あいう"をKEY_Sというキーで保存
+    [userDefaults synchronize];  // NSUserDefaultsに即時反映させる（即時で無くてもよい場合は不要）
     
     // NSUserDefaultsからデータを読み込む
-    NSInteger keyI = [ud integerForKey:@"KEY_A"];  // KEY_Iの内容をint型として取得
-    NSLog(@"%ld",(long)keyI);
-    double keyD = [ud doubleForKey:@"KEY_B"];  // KEY_Dの内容をdouble型として取得
-    NSLog(@"%.2f",keyD);
-    NSString *keyS = [ud stringForKey:@"KEY_C"];  // KEY_Sの内容をNSString型として取得
-    NSLog(@"%@",keyS);
+    NSInteger userDefaultseyI = [userDefaults integerForKey:@"KEY_A"];  // KEY_Iの内容をint型として取得
+    NSLog(@"%ld",(long)userDefaultseyI);
+    double userDefaultseyD = [userDefaults doubleForKey:@"KEY_B"];  // KEY_Dの内容をdouble型として取得
+    NSLog(@"%.2f",userDefaultseyD);
+    NSString *userDefaultseyS = [userDefaults stringForKey:@"KEY_C"];  // KEY_Sの内容をNSString型として取得
+    NSLog(@"%@",userDefaultseyS);
 }
 
 @end
